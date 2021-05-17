19ddb096ea27e4237b2728c2cc9e6617032f7743
Proxlight <69844328+Proxlight@users.noreply.github.com>
2021-05-17 14:47:34 +0530

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,9 @@
+from typing import Text
+from google_trans_new import google_translator
+import streamlit as st
+translator = google_translator()
+
+st.title("Language Translator :smile:")
+text=st.text_input("Enter your text here:")
+translate=translator.translate(text,lang_tgt="fr")
+st.write(translate)
